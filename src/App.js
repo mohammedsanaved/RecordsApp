2f2c3eb0d9e4f5b25a6fa90289bbee9bfc77a9ca
Mohammed Sanaved <mosanaved@gmail.com>
2023-03-02 02:45:25 +0530

    update last

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
+import TextField from "@mui/material/TextField";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import AddIcon from "@mui/icons-material/Add";
+import { useState, useEffect } from "react";
+// import Fields from "./components/Fields";
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [data, setData] = useState([]);
+  const [btn, setBtn] = useState(true);
+
+  useEffect(() => {
+    if (name !== "" && email !== "") {
+      setBtn(false);
+    } else {
+      setBtn(true);
+    }
+
+    // window.alert("Use effect");s
+  }, [name, email]);
+
+  const addData = () => {
+    if (name === "" || email === "") {
+      setBtn(false);
+      return;
+    }
+
+    setData([...data, { name, email }]);
+    setName("");
+    setEmail("");
+    // setBtn(true);
+  };
+  const removeItem = (index) => {
+    let arr = data;
+    arr.splice(index, 1);
+    setData([...arr]);
+  };
+  const handlePress = (event) => {
+    if (event.key === "Enter") {
+      addData();
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="form">
+        <Stack spacing={2} direction="row">
+          <TextField
+            onKeyPress={(e) => handlePress(e)}
+            className="text"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+          />
+          <TextField
+            onKeyPress={(e) => handlePress(e)}
+            className="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+          />
+          <Button
+            onClick={addData}
+            variant="contained"
+            color="success"
+            className="addBtn"
+            disabled={btn}
+          >
+            <AddIcon />
+          </Button>
+        </Stack>
+      </div>
+      <div className="data">
+        <div className="data_val">
+          <h4>Name</h4>
+          <h4>Email</h4>
+          <h4>Remove</h4>
+        </div>
+        {data.map((element, index) => {
+          return (
+            <div key={index} className="data_val">
+              <h4>{element.name}</h4>
+              <h4>{element.email}</h4>
+              <Stack>
+                <Button
+                  onClick={() => removeItem(index)}
+                  variant="contained"
+                  color="error"
+                >
+                  <DeleteIcon />
+                </Button>
+              </Stack>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Keep Recording</h1>
+    </div>
+  );
+};
+
+export default Header;
